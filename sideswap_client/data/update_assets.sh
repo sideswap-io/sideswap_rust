be65dd5f104e6ada4e58cc67b15cfd77d1f13370
SideSwap <hello@sideswap.io>
2025-03-21 07:31:47 +0400

    Minor update in update_assets.sh
    Fail the script if the git repo is not clean.

--- sideswap_client/data/update_assets.sh
+++ sideswap_client/data/update_assets.sh
@@ -1,5 +1,4 @@
 #!/bin/bash
-
 set -o errexit -o pipefail -o noclobber -o nounset
 
 cd "$(dirname "$0")"
@@ -8,3 +7,12 @@ rm -f ./assets.json
 rm -f ./assets-testnet.json
 echo '{"id":1, "method": "assets", "params": {"embedded_icons":false,"all_assets":true}}' | websocat --buffer-size 1048576 wss://api.sideswap.io/json-rpc-ws | jq '.result.assets | [.[] | select(.always_show == true)]' > ./assets.json
 echo '{"id":1, "method": "assets", "params": {"embedded_icons":false,"all_assets":true}}' | websocat --buffer-size 1048576 wss://api-testnet.sideswap.io/json-rpc-ws | jq '.result.assets | [.[] | select(.always_show == true)]' > ./assets-testnet.json
+
+GIT_STATUS="$(git status --porcelain)"
+if [[ -n "$GIT_STATUS" ]]; then
+    echo "Make sure the default assets are up to date!"
+    echo "Git status: $GIT_STATUS"
+    exit 1
+else
+    echo "Assets are up to date"
+fi
